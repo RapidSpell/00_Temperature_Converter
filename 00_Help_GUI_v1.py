00aed9e5d74a4c423d4ab1a197312be631bbd0a6
RapidSpell <142364086+RapidSpell@users.noreply.github.com>
2026-02-19 12:55:41 +1300

    Add files via upload

--- 00_Help_GUI_v1.py
+++ 00_Help_GUI_v1.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from functools import partial # To prevent unwanted windows
 
 
 class Converter:
@@ -25,16 +26,28 @@ class Converter:
 
 
     def to_help(self):
-        DisplayHelp()
+        """
+        Opens dialogue box and disables help button
+        (so that users can't create multiple help tabs)
+        """
+        DisplayHelp(self)
 
 
 class DisplayHelp:
 
-    def __init__(self):
+    def __init__(self, partner):
 
         # setup dialogue box and background color
         background = "#FFE6CC"
         self.help_box = Toplevel()
+
+        # Disable help button
+        partner.to_help_button.config(state=DISABLED)
+
+        # if users press cross at the top, coses help and
+        # 'releases' help button
+        self.help_box.protocol("WM_DELETE_WINDOW",
+                               partial(self.close_help, partner))
 
         self.help_frame = Frame(self.help_box,
                                 height=200)
@@ -45,15 +58,23 @@ class DisplayHelp:
                                         font=("Arial", 14, "bold"),)
         self.help_heading_label.grid(row=0)
 
+        help_text = """
+help text goes here
+and here
+and here
+and more
+        """
+
         self.help_text_label = Label(self.help_frame,
-                                     text="help text will go here", wraplength=350,
+                                     text=help_text, wraplength=350,
                                      justify="left")
         self.help_text_label.grid(row=1, padx=10)
 
         self.dismiss_button = Button(self.help_frame,
                                      font=("Arial", 12, "bold"),
                                      text="Dismiss", bg="#CC6600",
-                                     fg="#FFFFFF", command=self.close_help)
+                                     fg="#FFFFFF",
+                                     command=partial(self.close_help, partner))
         self.dismiss_button.grid(row=2, padx=10, pady=10)
 
         # List and loop to set background color on
@@ -65,7 +86,12 @@ class DisplayHelp:
             item.config(bg=background)
 
 
-    def close_help(self):
+    def close_help(self, partner):
+        """
+        Closes help dialogue box (and enables help button)
+        """
+        # Put help button back to normal...
+        partner.to_help_button.config(state=NORMAL)
         self.help_box.destroy()
 
 
---
+++ 00_Temperature_Converter_V5.py
@@ -0,0 +1,128 @@
+from tkinter import *
+import all_constants as c
+import conversion_rounding as cr
+
+
+class Converter:
+    """
+    Temperature conversion tool ( Celsius to Fahrenheit or vice versa )
+    """
+
+    def __init__(self):
+        """
+        Temperature converter GUI
+        """
+
+        self.all_calculations_list = []
+
+        self.temp_frame = Frame(padx=10, pady=10)
+        self.temp_frame.grid()
+
+        self.temp_heading = Label(self.temp_frame,
+                                  text="Temperature Converter",
+                                  font="Arial 16 bold"
+                                  )
+        self.temp_heading.grid(row=0)
+
+        instructions = ("please enter a temperature below and then press"
+                        "one of the buttons to convert it from centigrade"
+                        "to Fahrenheit or vice versa.")
+        self.temp_instructions = Label(self.temp_frame,
+                                       text=instructions,
+                                       wraplength=250, width=40,
+                                       justify="left")
+        self.temp_instructions.grid(row=1)
+
+        self.temp_entry = Entry(self.temp_frame,
+                                font="Arial 14"
+                                )
+        self.temp_entry.grid(row=2, padx=10, pady=10)
+
+        error = "Please enter a number"
+        self.answer_error = Label(self.temp_frame, text=error,
+                                  fg="#004c99",
+                                  font="Arial 16")
+        self.answer_error.grid(row=3)
+
+        #conversion, help and history / export button
+        self.button_frame = Frame(self.temp_frame)
+        self.button_frame.grid(row=4)
+
+        # button list (button text | bg color | row | column)
+        button_details_list = [
+            ["To Celsius", "#990099", lambda:self.check_temp(c.ABS_ZERO_FAHRENHEIT), 0, 0],
+            ["To Fahrenheit", "#009900", lambda:self.check_temp(c.ABS_ZERO_CELSIUS), 0, 1],
+            ["Help / info", "#cc6600", "", 1, 0],
+            ["History / Export", "#004c99", "", 1, 1],
+        ]
+
+        # list to hold buttons oce they have been made
+        self.button_ref_list = []
+
+        for item in button_details_list:
+            self.make_button = Button(self.button_frame,
+                                      text=item[0], bg=item[1],
+                                      fg="#ffffff", font="Arial 12 bold",
+                                      width=12, command=item[2])
+            self.make_button.grid(row=item[3], column=item[4], padx=5, pady=5)
+
+            self.button_ref_list.append(self.make_button)
+
+        # retrieve 'history / export' button and disable it at the start
+        self.to_history_button = self.button_ref_list[3]
+        self.to_history_button.config(state=DISABLED)
+
+
+    def check_temp(self,min_temp):
+        # retrieve temperature to be converted
+        to_convert = self.temp_entry.get()
+
+        #checks that amount to be converted is a number above absolute zero
+        try:
+            to_convert = float(to_convert)
+            if to_convert >= min_temp:
+                error = ""
+                self.convert(min_temp, to_convert)
+
+            else:
+                error= f"please enter a number {min_temp} or above"
+
+        except ValueError:
+            error = "please enter a number"
+
+        if error != "":
+            self.answer_error.config(text=error, fg="#9c0000")
+            self.temp_entry.config(bg="#f4CCCC")
+
+        else:
+            self.answer_error.config(fg="#004c99")
+            self.temp_entry.config(bg="#ffffff")
+
+        self.temp_entry.delete(0, END)
+
+
+    def convert(self, min_temp, to_convert):
+
+        if min_temp == c.ABS_ZERO_CELSIUS:
+            answer = cr.to_fahrenheit(to_convert)
+            answer_statement = f"{to_convert}°C is {answer}°F"
+            self.answer_error.config(text=f"{to_convert}°C is {answer}°F")
+
+        else:
+            answer = cr.to_celsius(to_convert)
+            answer_statement = f"{to_convert}°F is {answer}°C"
+
+        # Enable history_button as soon as we have a valid calculation
+        self.to_history_button.config(state=NORMAL)
+
+        self.answer_error.config(text=answer_statement, fg="#004c99")
+        self.all_calculations_list.append(answer_statement)
+        print(self.all_calculations_list)
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Converter")
+    Converter()
+    root.mainloop()
